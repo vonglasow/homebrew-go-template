8cabd011bde474cbebbaaaf3755e10a0798b7e34
Alexis von Glasow <vonglasow@gmail.com>
2024-04-07 08:59:47 +0200

    fix: name bin

--- Formula/template-go.rb
+++ Formula/template-go.rb
@@ -5,7 +5,7 @@ class TemplateGo < Formula
   license "GPL-3.0-or-later"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/vonglasow/template-go/releases/download/v1.1.2/gaia_1.1.2_darwin_arm64.tar.gz"
+    url "https://github.com/vonglasow/template-go/releases/download/v1.1.2/template-go_1.1.2_darwin_arm64.tar.gz"
     sha256 "faf8b374574e599fd3d4fc3d1a7f170946aa12e64e7ee5840628ce7f8a46daed"
   end
 
